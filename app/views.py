bea9988d22156f026178abba265fe64ac904fba3
dbjennings <dbjennings@gmail.com>
2021-03-06 15:58:15 -0600

    Base URLConf implemented

--- app/views.py
+++ app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class HomeView(TemplateView):
+    template_name = 'app/home.html'
+
+class LoginView(TemplateView):
+    template_name = 'app/login.html'
+
+class RegisterView(TemplateView):
+    template_name = 'app/register.html'
+
+class InboxView(TemplateView):
+    template_name = 'app/inbox.html'
+
+class ProjectView(TemplateView):
+    template_name = 'app/project.html'
+
+class LandingView(TemplateView):
+    template_name = 'app/landing.html'
