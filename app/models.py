1faa1911ea1a165219d70c4c31c568d3eebb0751
dbjennings <dbjennings@gmail.com>
2021-03-07 13:51:34 -0600

    Added user tracking to Task/Project

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.conf import settings
 
 class Tag(models.Model):
     tag_name = models.CharField(max_length=20)
@@ -29,9 +30,18 @@ class Task(models.Model):
     section = models.ForeignKey(Section, on_delete=models.CASCADE, null=True)
     project = models.ForeignKey(Project, on_delete=models.CASCADE, null=True)
     completed = models.BooleanField('completed',default=False)
-    date_created = models.DateTimeField('date created', default='django.utils.timezone.now')
-    date_scheduled = models.DateTimeField('date scheduled', null=True)
-    date_completed = models.DateTimeField('date completed', null=True)
+    # !!! Remove null=True when user functionality added
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
+    created_date = models.DateTimeField('date created')
+    modified_date = models.DateTimeField('date modified')
+    scheduled_date = models.DateTimeField('date scheduled', null=True)
+    completed_date = models.DateTimeField('date completed', null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.created_date = timezone.now()
+        self.modified_date = timezone.now()
+        return super(Task, self).save(*args, **kwargs)
     
     def __str__(self) -> str:
         return self.task_name
